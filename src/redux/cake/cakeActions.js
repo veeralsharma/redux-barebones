f19994a4ba4df70e807ca45b117ffc5e180b6a3e
veeralsharma <veeralsharma0001@gmail.com>
2021-07-17 14:35:18 +0530

    first

---
+++ src/redux/cake/cakeActions.js
@@ -0,0 +1,7 @@
+
+export const buyCake = (number = 1) => {
+  return {
+    type: 'BUY_CAKE',
+    payload: number
+  }
+}
---
+++ src/redux/user/userActions.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+export const fetchUsers = () => {
+  return (dispatch) => {
+    dispatch(fetchUsersRequest())
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        // response.data is the users
+        const users = response.data
+        dispatch(fetchUsersSuccess(users))
+      })
+      .catch(error => {
+        // error.message is the error message
+        dispatch(fetchUsersFailure(error.message))
+      })
+  }
+}
+
+export const fetchUsersRequest = () => {
+  return {
+    type:'FETCH_USERS_REQUEST'
+  }
+}
+
+export const fetchUsersSuccess = users => {
+  return {
+    type: 'FETCH_USERS_SUCCESS',
+    payload: users
+  }
+}
+
+export const fetchUsersFailure = error => {
+  return {
+    type: 'FETCH_USERS_FAILURE',
+    payload: error
+  }
+}
